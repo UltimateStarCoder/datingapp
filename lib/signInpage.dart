8deefc31978e68a5f76f31c474d9dc814ecd769a
Mac <onyimacr@gmail.com>
2022-10-03 00:02:07 -0600

    Unversioned Files

---
+++ lib/signInpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          SizedBox(height: 10,)
+
+        ],
+      ),
+
+    );
+  }
+}
